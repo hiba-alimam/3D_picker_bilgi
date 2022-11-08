08de35aeef794ca0687d4592a52e18344177d297
STUNETWORKS\student <space.obsession1@gmail.com>
2022-11-08 15:13:44 +0300

    session 3 UI scripts

---
+++ Assets/Scripts/Controllers/UIPanelController.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class UIPanelController : MonoBehaviour
+{
+    #region Self Variables
+
+    #region Serialized Variables
+
+    [SerializeField] private List<Transform> layers = new List<Transform>();
+
+    #endregion
+
+    #endregion
+    private void OnEnable()
+    {
+        SubscribeEvents();
+    }
+
+    private void SubscribeEvents()
+    {
+
+    }
+
+    private void UnsubscribeEvents()
+    {
+
+    }
+
+    private void OnDisable()
+    {
+        UnsubscribeEvents();
+
+    }
+
+    private void OnOpenPanel(UIPanelTypes type, int layerValue)
+    {
+        OnClosePanel(layerValue);
+        Instantiate(Resources.Load<GameObject>($"Screens/{type}Panel"), layers[layerValue]);
+    }
+
+    private void OnClosePanel(int layerValue)
+    {
+        if (layers[layerValue].childCount > 0)
+        {
+            Destroy(layers[layerValue].GetChild(0).gameObject);
+        }
+    }
+
+    private void OnCloseAllPanels()
+    {
+        for (int i = 0; i < layers.Count; i++)
+        {
+            if (layers[i].childCount > 0)
+            {
+                Destroy(layers[i].GetChild(0).gameObject);
+            }
+        }
+}
